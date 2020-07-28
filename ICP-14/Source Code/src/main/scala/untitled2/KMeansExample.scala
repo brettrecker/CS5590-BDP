e3e77392f6e27d92b57e788db16a404934606e20
Recker <BR030614@cerner.net>
2020-07-28 17:34:44 -0500

    ICP-14

---
+++ ICP-14/Source Code/src/main/scala/untitled2/KMeansExample.scala
@@ -0,0 +1,40 @@
+package untitled2
+
+import org.apache.spark.{SparkConf, SparkContext}
+// $example on$
+import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.linalg.Vectors
+// $example off$
+
+object KMeansExample {
+
+  def main(args: Array[String]) {
+
+
+
+    val conf = new SparkConf().setAppName("Samples").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    // $example on$
+    // Load and parse the data
+    val data = sc.textFile("C:\\Users\\br030614\\Documents\\untitled2\\kmeans_data.txt")
+    val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
+
+    // Cluster the data into two classes using KMeans
+    val numClusters = 2
+    val numIterations = 20
+    val clusters = KMeans.train(parsedData, numClusters, numIterations)
+
+    // Evaluate clustering by computing Within Set Sum of Squared Errors
+    val WSSSE = clusters.computeCost(parsedData)
+    println(s"Within Set Sum of Squared Errors = $WSSSE")
+
+    // Save and load model
+    clusters.save(sc, "target/tmp/KMeansExample/KMeansModel")
+    val sameModel = KMeansModel.load(sc, "target/tmp/KMeansExample/KMeansModel")
+    // $example off$
+
+    sc.stop()
+  }
+}
+// scalastyle:on println
